5d8bae081344b108121e9c6636e593e742c71b7e
MaximoAReynoso <maximoreynosob@gmail.com>
2026-09-04 07:18:55 -0300

    TOKENS defined

--- lex/yylex.c
+++ lex/yylex.c
@@ -1,10 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define STRING_MULTIPLINEA 257
-#define COMENTARIO 258
-#define SUMA 259
+#ifndef TOKENS
 
+#define PLUS 257
+#define MINUS 258
+#define MULT 259
+#define DIV 260
+#define ASSIGN_COLON 261
+#define ASSIGN 262
+
+#define GE 263
+#define LE 264
+#define GT 265
+#define LT 266
+#define EQ 267
+#define NE 268
+
+#define LPAREN 269
+#define RPAREN 270
+
+#define COMMA 271
+#define SEMICOLON 272
+#define AND 273
+#define COLON 274
+
+#endif
 
 int yylex(FILE *file){
     if (file == NULL){
