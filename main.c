913825561ce2da185df1c87ef0d83cbfb96018d7
MaximoAReynoso <maximoreynosob@gmail.com>
2026-09-04 07:54:50 -0300

    main avanzado

--- main.c
+++ main.c
@@ -1,5 +1,26 @@
 #include <stdio.h>
-int main(){
-    printf("Hola Mundo!\n");
+
+void yyparse();
+
+int main(int argc, char* argv[]){
+    if (argc != 2){
+        if (argc < 2)
+            printf("Error - Necesita un archivo a compilar.");
+        else
+            printf("Error - Necesita un unico archivo a compilar.");
+        return 1;
+    }
+
+    char* file_path = argv[1];
+    FILE *file = fopen(file_path, "r");
+
+    if (file == NULL){
+        printf("Error - Archivo no encontrado.");
+        return 1;
+    }
+
+    //yyparse();
+
+    fclose(file);
     return 0;
 }
