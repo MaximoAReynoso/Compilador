c3199a79ae5c2d8a95c1f6673f5f6fe26ef553bc
MaximoAReynoso <maximoreynosob@gmail.com>
2026-09-04 07:30:13 -0300

    TOKENS defined HECHO

--- lex/yylex.c
+++ lex/yylex.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 
 #ifndef TOKENS
-
 #define PLUS 257
 #define MINUS 258
 #define MULT 259
@@ -25,6 +24,34 @@
 #define AND 273
 #define COLON 274
 
+#define ID 275
+#define CTE 276
+
+#define IF 278
+#define ELSE 279
+#define END_IF 280
+#define BEGIN 281
+#define END 282
+#define POUT 283
+#define RET 284
+#define CLASS 285
+#define FUNCT 286
+
+#define PES_I 287
+#define SINGLEF 288
+#define MULT_STRING 289
+#define COMMENT 290
+
+#define FROM 291
+#define TO 292
+#define BY 293
+#define REPEAT 294
+
+#define LBRACKET 295
+#define RBRACKET 296
+
+#define COMPTIME 297
+#define TOSF 298
 #endif
 
 int yylex(FILE *file){
